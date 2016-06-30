63108640e61f91fdf5c5ae03d3b95549c8bd1f72
sasho-i <sashomail@gmail.com>
2016-07-01 00:33:39 +0200

    Fixed some formatting.

--- IntroductionToProgramming/PrintNumbers/PrintNumbers.cs
+++ IntroductionToProgramming/PrintNumbers/PrintNumbers.cs
@@ -6,9 +6,9 @@ namespace PrintNumbers
     {
         static void Main()
         {
-            Console.WriteLine("1");
-            Console.WriteLine("101");
-            Console.WriteLine("1001");
+            Console.WriteLine(1);
+            Console.WriteLine(101);
+            Console.WriteLine(1001);
         }
     }
 }
